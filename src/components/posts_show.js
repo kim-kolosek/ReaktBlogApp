e0739973bcea847e5d6134c5e6c0422ef3177239
NenadSSS <nenad_savovic@yahoo.com>
2016-07-22 07:06:53 +0200

    Last commit

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
 import { fetchPost } from '../actions/index';
+import { deletePost } from '../actions/index';
 
 class PostsShow extends Component{
 
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
   componentWillMount(){
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick(){
+    this.props.deletePost(this.props.params.id)
+    .then(()=> {
+      //blog post has been created, navigate the user to index page
+      //by calling this.context.router.push with the new path to navigate to
+      this.context.router.push("/")
+     });
   }
 
   render(){
@@ -18,6 +32,10 @@ class PostsShow extends Component{
 
     return(
       <div>
+        <Link to="/" className="">Back to Index</Link>
+        <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger pull-xs-right">
+          Delete Post
+        </button>
         <h3>{this.props.post.title}</h3>
         <h6>Categories: {this.props.post.categories}</h6>
         <p>
@@ -33,9 +51,9 @@ function mapStateToProps(state){
   return {post: state.posts.post }
 }
 
-function mapDispatchToProps(dispatch ){
-  return bindActionCreators({ fetchPost : fetchPost }, dispatch);
-}
+// function mapDispatchToProps(dispatch ){
+//   return bindActionCreators({ fetchPost : fetchPost }, dispatch);
+// }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
